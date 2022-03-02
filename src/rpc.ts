5198811cef29688b8271d8772b76fabbd5dbff6b
Zack Skolnik <zskolnik@protonmail.com>
2022-03-02 05:25:13 -0800

    feat: add cache, add latest rates, fix typescript errors

--- src/rpc.ts
+++ src/rpc.ts
@@ -1,51 +1,73 @@
-import fetch from 'node-fetch';
 import { ethers } from "ethers";
+import fetch from "node-fetch";
+import { CacheContainer } from 'node-ts-cache'
+import { MemoryStorage } from 'node-ts-cache-storage-memory'
 
-// TODO: compile v1-periphery to typechain and put in SDK repo
-// import { Margin } from "../../typechain/Margin";
+const cache = new CacheContainer(new MemoryStorage())
+
+// TODO: import Margin entity
 
 
 // REST calls to an external API
 
-const getRates = async (symbol: string) => {
-    // Establish parameters
+const getHistoricalRates = async (symbol: string) => {
     // Supported symbols: cDAI, cUSDC, aDAI, aUSDC
     const startTime = new Date("2021-06-01T00:00:00Z").toISOString();
     const endTime = new Date().toISOString();
-    const resolution = "1H";
-
-    // Build the query
-    const base = `http://app.sense.finance/api/v0.1/rates/${symbol}/timeseries`;
+    const resolution = "1D";
+    const base = `https://app.sense.finance/api/v0.1/rates/${symbol}/timeseries`;
     const query = `${base}?from=${startTime}&to=${endTime}&resolution=${resolution}`;
+    // console.log(query);
+    const cachedRates = await cache.getItem<string[]>(query);
+    if (cachedRates) {
+        return cachedRates;
+    }
+    try {
+        const rates = await fetch(query).then((resp: any) => resp.json());
+        await cache.setItem(query, rates, { isCachedForever: true });   // ttl?
+        return rates;
+    } catch (err) {
+        console.error(err);
+    }
+}
 
-    // Fetch
-    const rates = await fetch(query).then(resp => resp.json());
+const getLatestRates = async (symbol: string) => {
+    // Supported symbols: cDAI, cUSDC, aDAI, aUSDC
+    const query = `https://app.sense.finance/api/v0.1/rates/${symbol}/latest`;
+    const cachedRates = await cache.getItem<string[]>(query);
+    if (cachedRates) {
+        console.log(`Cache hit for key ${query}`);
+        return cachedRates;
+    }
+    const rates = await fetch(query).then((resp: any) => resp.json());
+    // const rates = await fetch(query);
+    await cache.setItem(query, rates, { ttl: 60 });
     return rates;
 }
 
 
 // JSONRPC calls to an external contract or the blockchain itself
 
-const getProvider = async () => {
-    const provider = new ethers.providers.JsonRpcProvider();
+const getProvider = async (network: string) => {
+    const provider = new ethers.providers.JsonRpcProvider(network);
     return provider;
 }
 
-const getCurrentBlock = async () => {
-    const provider = await getProvider();
+const getCurrentBlock = async (network: string) => {
+    const provider = await getProvider(network);
     const blockNumber = await provider.getBlockNumber();
     return blockNumber;
 }
 
-const getWalletBalance = async (address: string) => {
-    const provider = await getProvider();
+const getWalletBalance = async (network: string, address: string) => {
+    const provider = await getProvider(network);
     const balanceInWei = await provider.getBalance(address);
     const balanceInEth = ethers.utils.formatEther(balanceInWei);
     return balanceInEth;
 }
 
-const getEnsName = async (address: string) => {
-    const provider = await getProvider();
+const getEnsName = async (network: string, address: string) => {
+    const provider = await getProvider(network);
     const name = await provider.lookupAddress(address);
     if (name) {
         const resolvedAddress = await provider.resolveName(name);
@@ -55,14 +77,14 @@ const getEnsName = async (address: string) => {
     }
 }
 
-const getAddressFromEnsName = async (name: string) => {
-    const provider = await getProvider();
+const getAddressFromEnsName = async (network: string, name: string) => {
+    const provider = await getProvider(network);
     const address = await provider.resolveName(name);
     return address;
 }
 
-const getAvatar = async (address: string) => {
-    const provider = await getProvider();
+const getAvatar = async (network: string, address: string) => {
+    const provider = await getProvider(network);
     const avatar = await provider.getAvatar(address);
     return avatar;
 }
@@ -89,19 +111,17 @@ const getMinimumMarginRequirement = async (
 // }
 
 
-// debugging
-
 void async function () {
-    console.log('Hello, World')
+    const network = "https://eth-mainnet.alchemyapi.io/v2/pNmKK8pTXHVggw2X4XPAOOuL9SllmxdZ";
     const ensName = "vitalik.eth";
-    console.log(const address = await getAddressFromEnsName(ensName);
-    console.log(await getRates("cDAI"));
-    console.log(await getCurrentBlock());
-    console.log(await getWalletBalance(ensName));
-    console.log(await getEnsName(address));
-    console.log();
-    console.log();
-    console.log();
-    console.log();
-    console.log(await getCurrentBlock());
+    console.log(`Current block: ${await getCurrentBlock(network)}`);
+    let address = await getAddressFromEnsName(network, ensName);
+    console.log(`Address: ${address}`);
+    address = address || "0xd8da6bf26964af9d7eed9e03e53415d37aa96045";
+    console.log(`Wallet balance: ${await getWalletBalance(network, ensName)}`);
+    console.log(`ENS name: ${await getEnsName(network, address)}`);
+    console.log(`Avatar: ${await getAvatar(network, address)}`);
+    console.log(`Historical Rates: %j`, await getHistoricalRates("cDAI"));
+    console.log(`Latest Rates: %j`, await getLatestRates("cDAI"));
+    console.log(`Latest Rates: %j`, await getLatestRates("cDAI"));
 }();
