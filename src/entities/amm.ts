0154069609b233e2c5f5fcd74f918566d3a6ec37
tudor47 <tudorcostin7@gmail.com>
2022-08-04 14:51:57 +0300

    feat: integrate IDs 5, 6

--- src/entities/amm.ts
+++ src/entities/amm.ts
@@ -2197,6 +2197,14 @@ class AMM {
         prefix = "r";
         break;
       }
+      case 5: {
+        prefix = "a";
+        break;
+      }
+      case 6: {
+        prefix = "c";
+        break;
+      }
       default: {
         throw new Error("Unrecognized protocol");
       }
@@ -2945,7 +2953,8 @@ class AMM {
     const blockPerHour = 274;
 
     switch (this.rateOracle.protocolId) {
-      case 1: {
+      case 1: 
+      case 5: {
         const lastBlock = await this.provider.getBlockNumber();
         const oneBlockAgo = BigNumber.from((await this.provider.getBlock(lastBlock - 1)).timestamp);
         const twoBlocksAgo = BigNumber.from((await this.provider.getBlock(lastBlock - 2)).timestamp);
@@ -2957,7 +2966,8 @@ class AMM {
         return oneWeekApy.div(BigNumber.from(1000000000000)).toNumber() / 1000000;
       }
 
-      case 2: {
+      case 2: 
+      case 6: {
         const daysPerYear = 365;
 
         const fcmContract = fcmCompoundFactory.connect(this.fcmAddress, this.provider);
