e2bbbdda0b7922fa836a74f2ecf69f64e65ff27c
Soleiman Anwary <soleiman.anwary@gmail.com>
2022-11-17 22:39:10 +0000

    fix: refactor getAccruedCashflow.spec.ts to match mocha/chai typings
    	- Add tests to mellowLpRouter.test.ts
    	- Move getAccruedCashflow.test.ts to tests directory and delete /entities/__tests__

--- tests/mellowLpRouter.test.ts
+++ tests/mellowLpRouter.test.ts
@@ -7,10 +7,13 @@ import MellowLpRouter from '../src/entities/mellowLpRouter';
 import { abi as MellowMultiVaultRouterABI } from '../src/ABIs/MellowMultiVaultRouterABI.json';
 
 const { provider } = waffle;
-let mellowLpRouter: MellowLpRouter;
+let ethMellowLpRouter: MellowLpRouter;
+let erc20MellowLpRouter: MellowLpRouter;
 
-const mellowRouterAddress = '';
-const defaultWeights: number[] = [];
+const depositAmount = 1; // Set a default 1 ETH constant for use in tests;
+const ethMellowRouterAddress = '';
+const erc20MellowRouterAddress = '';
+const defaultWeights: number[] = [0.5, 0.5]; // default even split between 2 pools
 
 const signer = new Wallet(
   '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
@@ -21,61 +24,101 @@ const userWallet = signer;
 
 describe('Mellow Router Test Suite', () => {
   before('Setting up the suite', async () => {
-    mellowLpRouter = new MellowLpRouter({
-      mellowRouterAddress: mellowRouterAddress,
+    ethMellowLpRouter = new MellowLpRouter({
+      mellowRouterAddress: ethMellowRouterAddress,
       defaultWeights: defaultWeights,
       provider,
     });
 
+    erc20MellowLpRouter = new MellowLpRouter({
+      mellowRouterAddress: erc20MellowRouterAddress,
+      defaultWeights: defaultWeights,
+      provider,
+    });
     // Initialise the mellow vault
-    await mellowLpRouter.vaultInit();
+    await ethMellowLpRouter.vaultInit();
 
     // Initialise the user so the router contract is connected with user to keep track of them as a signer for the deposits
-    await mellowLpRouter.userInit(userWallet);
-  });
-  it('Dummy test', () => {
-    expect(1).to.be.eq(1);
-  });
-  it('Check that vault has been initialised correctly', async () => {
-    const mellowRouter = mellowLpRouter;
-    expect(mellowRouter.vaultInitialized).to.be.eq(true);
-
-    // Get the router contract (we can get this from a json once everything is deployed)
-    const mellowRouterContract = new ethers.Contract(
-      mellowRouterAddress,
-      MellowMultiVaultRouterABI,
-      provider,
-    );
-
-    const tokenAddress = await mellowRouterContract.token();
-    expect(mellowRouter.readOnlyContracts?.token.address).to.be.eq(tokenAddress);
+    await ethMellowLpRouter.userInit(userWallet);
   });
 
-  it('Check that user has been initialised correctly', async () => {
-    const mellowRouter = mellowLpRouter;
+  describe('ETH POOLS', async () => {
+    it('Check that vault has been initialised correctly', async () => {
+      const mellowRouter = ethMellowLpRouter;
+      expect(mellowRouter.vaultInitialized).to.be.eq(true);
 
-    // Get the router contract
-    const mellowRouterContract = new ethers.Contract(
-      mellowRouterAddress,
-      MellowMultiVaultRouterABI,
-      provider,
-    );
+      // Get the router contract (we can get this from a json once everything is deployed)
+      const mellowRouterContract = new ethers.Contract(
+        ethMellowRouterAddress,
+        MellowMultiVaultRouterABI,
+        provider,
+      );
 
-    expect(mellowRouter.writeContracts?.mellowRouter.address).to.be.eq(
-      mellowRouterContract.address,
-    );
+      const tokenAddress = await mellowRouterContract.token();
+      expect(mellowRouter.readOnlyContracts?.token.address).to.be.eq(tokenAddress);
+    });
+
+    it('Check that user has been initialised correctly', async () => {
+      const mellowRouter = ethMellowLpRouter;
+
+      // Get the router contract
+      const mellowRouterContract = new ethers.Contract(
+        ethMellowRouterAddress,
+        MellowMultiVaultRouterABI,
+        provider,
+      );
+
+      expect(mellowRouter.writeContracts?.mellowRouter.address).to.be.eq(
+        mellowRouterContract.address,
+      );
+    });
+
+    it('User deposits eth into router contract with uneven split', async () => {
+      const mellowRouter = ethMellowLpRouter;
+      const weights = [0.2, 0.8]; // Need to know how many different maturities there are to know the split
+
+      await mellowRouter.deposit(depositAmount, weights);
+
+      const userWalletBalanceBefore = await userWallet.getBalance();
+      const userWalletBalanceAfter = userWalletBalanceBefore.toNumber() - 1;
+
+      expect(userWalletBalanceBefore).to.be.eq(userWalletBalanceAfter);
+    });
+    it('User deposits eth into router contract 1:0 split', async () => {
+      const mellowRouter = ethMellowLpRouter;
+      const weights = [1, 0];
+
+      await mellowRouter.deposit(depositAmount, weights);
+    });
+
+    it('User deposits eth into router without specifying weights', async () => {
+      const mellowRouter = ethMellowLpRouter;
+
+      const receipt = await mellowRouter.deposit(depositAmount);
+      // expect the default weights to be used here and the transaction to not fail
+      expect(receipt).to.be.a('Promise<ContractReceipt>');
+    });
   });
 
-  it('User deposits funds into router contract with equal split', async () => {
-    const mellowRouter = mellowLpRouter;
-    const amount = ethers.constants.One.toNumber();
-    const weights = [0.2, 0.8]; // Need to know how many different maturities there are to know the split
+  describe('ERC20 DEPOSITS', async () => {
+    it('User deposits ERC20 into router with uneven split', async () => {
+      const mellowRouter = erc20MellowLpRouter;
+      const weights = [0.2, 0.8];
 
-    await mellowRouter.deposit(amount, weights);
+      await mellowRouter.deposit(depositAmount, weights);
+    });
 
-    const userWalletBalanceBefore = await userWallet.getBalance();
-    const userWalletBalanceAfter = userWalletBalanceBefore.toNumber() - 1;
+    it('User deposits ERC20 into router with 1:0 split', async () => {
+      const mellowRouter = erc20MellowLpRouter;
+      const weights = [1, 0];
 
-    expect(userWalletBalanceBefore).to.be.eq(userWalletBalanceAfter);
+      await mellowRouter.deposit(depositAmount, weights);
+    });
+
+    it('User deposits ERC20 into router without specifying weights', async () => {
+      const mellowRouter = erc20MellowLpRouter;
+
+      await mellowRouter.deposit(depositAmount);
+    });
   });
 });
