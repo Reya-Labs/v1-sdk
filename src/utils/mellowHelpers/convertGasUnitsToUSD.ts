ceb1ade43937b9bc9ddeb8c1a20f33f1f055fbf5
Soleiman Anwary <soleiman.anwary@gmail.com>
2022-12-22 20:40:50 +0000

    feat: add test for gas units to usd conversion and tidy up that function

--- src/utils/mellowHelpers/convertGasUnitsToUSD.ts
+++ src/utils/mellowHelpers/convertGasUnitsToUSD.ts
@@ -1,45 +1,17 @@
-/* eslint-disable no-await-in-loop */
-/* eslint-disable no-console */
-import axios from 'axios';
 import { BigNumber } from 'ethers';
-import { sentryTracker } from '../sentry';
+import { geckoEthToUsd } from './geckoEthToUsd';
+import { getGasPriceGwei } from './getGasPriceGwei';
 
-export async function convertGasUnitsToUSD(gasUnits: BigNumber): Promise<string> {
-  const geckoEthToUsd = async () => {
-    for (let attempt = 0; attempt < 5; attempt += 1) {
-      try {
-        const data = await axios.get(
-          `https://pro-api.coingecko.com/api/v3/simple/price?x_cg_pro_api_key=${process.env.REACT_APP_COINGECKO_API_KEY}&ids=ethereum&vs_currencies=usd`,
-        );
-        return data.data.ethereum.usd;
-      } catch (error) {
-        sentryTracker.captureException(error);
-        sentryTracker.captureMessage('Unsuccesful call to retrieve ETH/USD price from Coingecko');
-        console.error(error);
-      }
-    }
-    return 0;
-  };
-
-  const getGasPriceGwei = async () => {
-    for (let attempt = 0; attempt < 5; attempt += 1) {
-      try {
-        const data = await axios.get(`https://ethgasstation.info/api/ethgasAPI.json?`);
-        const averageGasPrice = data.data.average / 10;
-        return averageGasPrice;
-      } catch (error) {
-        sentryTracker.captureException(error);
-        sentryTracker.captureMessage(
-          'Unsuccesful call to retrieve gas price (gwei) price from ETH Gas Station',
-        );
-        console.error(error);
-      }
-    }
-    return 0;
-  };
-
+export async function convertGasUnitsToUSD(gasUnits: BigNumber): Promise<number> {
   const gasPriceGwei = await getGasPriceGwei();
   const ethToUSDPrice = await geckoEthToUsd();
-  const gasPriceUSD = gasUnits.mul(gasPriceGwei).mul(ethToUSDPrice).div('1000000000');
-  return gasPriceUSD.toString();
+  const ethToUSDPriceScaled = ethToUSDPrice * 100;
+
+  const gasPriceUSDScaledBN = gasUnits
+    .mul(BigNumber.from(gasPriceGwei))
+    .mul(BigNumber.from(ethToUSDPriceScaled));
+
+  const gasPriceUSDScaled = parseFloat(gasPriceUSDScaledBN.toString());
+  const gasPriceUSD = gasPriceUSDScaled / 1000000000000;
+  return gasPriceUSD;
 }
