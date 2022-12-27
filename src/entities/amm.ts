627af0de5133dc05159ca8f58619cb0ea2e8d5f9
tudor47 <tudorcostin7@gmail.com>
2022-12-27 22:14:36 +0200

    feat: decreased delta for getApy

--- src/entities/amm.ts
+++ src/entities/amm.ts
@@ -1930,7 +1930,7 @@ class AMM {
 
     // Get last block timestamp
     const block = await this.provider.getBlock("latest");
-    const currentTime = block.timestamp - 30;
+    const currentTime = block.timestamp - 1;
 
     // Get before maturity
     const beforeMaturity = currentTime < this.endDateTime.toSeconds();
