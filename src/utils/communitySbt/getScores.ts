292d4b02d674d44810fa66d97fba865398b7758d
tudor47 <tudorcostin7@gmail.com>
2023-01-06 19:28:52 +0200

    feat: still need to redo tests

--- src/utils/communitySbt/getScores.ts
+++ src/utils/communitySbt/getScores.ts
@@ -1,11 +1,8 @@
-import { getPositions } from '@voltz-protocol/subgraph-data';
-import { ONE_YEAR_IN_SECONDS } from '../../constants';
+import { getSeasonUsers } from '@voltz-protocol/subgraph-data';
 
 export type GetScoresArgs = {
-  seasonStart: number;
-  seasonEnd: number;
+  season: number;
   subgraphUrl: string;
-  ethPrice: number;
   ignoredWalletIds: Record<string, boolean>;
 };
 
@@ -20,79 +17,23 @@ export type ScoreResult = {
  * Score is based on swaps for Traders and mints/burns for LPs.
  */
 export async function getScores({
-  seasonStart,
-  seasonEnd,
+  season,
   subgraphUrl,
-  ethPrice,
   ignoredWalletIds,
 }: GetScoresArgs): Promise<ScoreResult> {
-  const positions = await getPositions(
-    subgraphUrl,
-    Date.now().valueOf(),
-    {
-      active: true,
-    },
-    {
-      history: true,
-    },
-  );
+  const seasonUsers = await getSeasonUsers(subgraphUrl, {
+    season,
+  });
 
   const traderScores: Record<string, number> = {};
   const lpScores: Record<string, number> = {};
 
-  positions.forEach((position) => {
-    if (!ignoredWalletIds[position.owner]) {
-      // Tracking trader scores
-      position.swaps.forEach((swap) => {
-        const actionTime = swap.creationTimestampInMS / 1000;
-        const termEnd = position.amm.termEndTimestampInMS / 1000;
-        const tokenPrice = position.amm.tokenName === 'ETH' ? ethPrice : 1;
-
-        if (seasonStart < actionTime && actionTime <= seasonEnd) {
-          const timeWeightedNotional =
-            (Math.abs(swap.variableTokenDelta) * (termEnd - actionTime)) / ONE_YEAR_IN_SECONDS;
-
-          const addScore = timeWeightedNotional * tokenPrice;
-          if (addScore > 0) {
-            traderScores[position.owner] = (traderScores[position.owner] || 0) + addScore;
-          }
-        }
-      });
-
-      // Tracking lp scores
-      position.mints.forEach((mint) => {
-        const actionTime = mint.creationTimestampInMS / 1000;
-        const termEnd = position.amm.termEndTimestampInMS / 1000;
-        const tokenPrice = position.amm.tokenName === 'ETH' ? ethPrice : 1;
-
-        if (seasonStart < actionTime && actionTime <= seasonEnd) {
-          const timeWeightedNotional =
-            (mint.liquidity * (termEnd - actionTime)) / ONE_YEAR_IN_SECONDS;
-
-          const addScore = timeWeightedNotional * tokenPrice;
-          if (addScore > 0) {
-            lpScores[position.owner] = (lpScores[position.owner] || 0) + addScore;
-          }
-        }
-      });
-
-      position.burns.forEach((burn) => {
-        const actionTime = burn.creationTimestampInMS / 1000;
-        const termEnd = position.amm.termEndTimestampInMS / 1000;
-        const tokenPrice = position.amm.tokenName === 'ETH' ? ethPrice : 1;
-
-        if (seasonStart < actionTime && actionTime <= seasonEnd) {
-          const timeWeightedNotional =
-            (burn.liquidity * (termEnd - actionTime)) / ONE_YEAR_IN_SECONDS;
-
-          const addScore = timeWeightedNotional * tokenPrice;
-          if (addScore > 0) {
-            lpScores[position.owner] = (lpScores[position.owner] || 0) - addScore;
-          }
-        }
-      });
-    }
-  });
+  seasonUsers
+    .filter((user) => !ignoredWalletIds[user.owner])
+    .forEach((user) => {
+      traderScores[user.owner] = user.timeWeightedTradedNotional;
+      lpScores[user.owner] = user.timeWeightedTradedNotional;
+    });
 
   return {
     traderScores,
