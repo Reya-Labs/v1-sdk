92f0ca3f6e7eb01fa37079dfc970ef5c62feec31
Alexandru Niculae <niculae.alexandru1@gmail.com>
2023-01-17 13:12:04 +0000

    fix: addressed comments

--- tests/mocha/utils.test.ts
+++ tests/mocha/utils.test.ts
@@ -1,21 +1,37 @@
 import { expect } from "chai";
-import { BigNumber } from "ethers";
-import { waffle } from "hardhat";
+import { waffle, network } from "hardhat";
 import { convertGasUnitsToUSD } from "../../src/utils/mellowHelpers/convertGasUnitsToUSD";
-import { getGasPriceGwei } from "../../src/utils/mellowHelpers/getGasPriceGwei";
-import { geckoEthToUsd } from "../../src/utils/priceFetch";
 
 const { provider } = waffle;
 
 describe('Test utils', () => {
+  const resetNetwork = async (blockNumber: number) => {
+    await network.provider.request({
+      method: 'hardhat_reset',
+      params: [
+        {
+          chainId: 5,
+          forking: {
+            jsonRpcUrl: process.env.GOERLI_URL,
+            blockNumber,
+          },
+        },
+      ],
+    });
+  };
+
+  beforeEach(async () => {
+    await resetNetwork(8321776);
+  });
+
   it('Gas Units to USD conversion function', async () => {
     expect(await convertGasUnitsToUSD(provider, 100000)).to.be.approximately(
-      3.953,
+      3.12,
       0.1,
     );
 
     expect(100000 * (await convertGasUnitsToUSD(provider, 1))).to.be.approximately(
-      3.953,
+      3.12,
       0.1,
     );
   });
