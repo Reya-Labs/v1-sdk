8f339a343fa147e43c08a68037a129af2ed4618a
tudor47 <tudorcostin7@gmail.com>
2023-01-20 19:59:29 +0200

    feat: partially tested

--- src/entities/mellow-stateless/mellow-routers/deposit.ts
+++ src/entities/mellow-stateless/mellow-routers/deposit.ts
@@ -3,92 +3,111 @@ import { MellowMultiVaultRouterABI } from '../../../ABIs';
 import { getGasBuffer } from '../../../constants';
 import { getTokenInfo } from '../../../services/getTokenInfo';
 import { scale } from '../../../utils/scaling';
-import { getMellowConfig } from '../config/config';
-import { validateWeights } from '../utils/validateWeights';
+import { getRouterConfig } from '../utils/getRouterConfig';
+import { mapWeights } from '../utils/mapWeights';
+import { RouterInfo } from '../getters/types';
+import { getOptimiserInfo } from '../getters/optimisers/getOptimiserInfo';
 
 type DepositArgs = {
   routerId: string;
-  tokenId: string;
   amount: number;
-  weights: [string, number][];
+  spareWeights: [string, number][];
   signer: ethers.Signer;
 };
 
-export const deposit = async ({
-  routerId,
-  tokenId,
-  amount,
-  weights,
-  signer,
-}: DepositArgs): Promise<ethers.ContractReceipt> => {
-  const config = getMellowConfig();
+type DepositResponse = {
+  transaction: {
+    receipt: ethers.ContractReceipt;
+  };
+  newRouterState: RouterInfo;
+};
 
-  const routerConfig = config.MELLOW_ROUTERS.find(
-    (item) => item.router.toLowerCase() === routerId.toLowerCase(),
-  );
-
-  if (!routerConfig) {
-    // TODO: add sentry
-    throw new Error('Router ID not found');
-  }
-
-  if (routerConfig.isVault) {
-    throw new Error('Deposit not supported for vaults.');
-  }
-
-  const routerVaultIds = routerConfig.vaults.map((v) => v.address);
-  const allWeights = routerVaultIds.map((routerVaultId) => {
-    const weight = weights.find((w) => w[0] === routerVaultId);
-    return weight ? weight[1] : 0;
-  });
-
-  if (validateWeights(allWeights)) {
-    // TODO: add sentry
-    throw new Error('Weights are invalid');
-  }
-
-  // Get the token decimals
-  const { decimals: tokenDecimals, name: tokenName } = getTokenInfo(tokenId);
-  const isETH = tokenName === 'ETH';
-
-  const scaledAmount = scale(amount, tokenDecimals);
+const getTransaction = async (
+  mellowRouter: ethers.Contract,
+  isETH: boolean,
+  scaledAmount: ethers.BigNumber,
+  weights: number[],
+): Promise<ethers.ContractTransaction> => {
   const tempOverrides: { value?: BigNumber; gasLimit?: BigNumber } = {};
 
   if (isETH) {
     tempOverrides.value = scaledAmount;
-  }
 
-  const mellowRouter = new ethers.Contract(routerId, MellowMultiVaultRouterABI, signer);
+    // Simulate deposit
+    try {
+      await mellowRouter.callStatic.depositEth(weights, tempOverrides);
+    } catch (error) {
+      // TODO: Add Sentry
+      throw new Error('Unsuccessful deposit simulation.');
+    }
+
+    // Estimate gas
+    const gasLimit = await mellowRouter.estimateGas.depositEth(weights, tempOverrides);
+    tempOverrides.gasLimit = getGasBuffer(gasLimit);
+
+    // Send transaction
+    return mellowRouter.depositEth(weights, tempOverrides);
+  }
 
   // Simulate deposit
   try {
-    if (isETH) {
-      await mellowRouter.callStatic.depositEth(weights, tempOverrides);
-    } else {
-      await mellowRouter.callStatic.depositErc20(scaledAmount, weights);
-    }
+    await mellowRouter.callStatic.depositErc20(scaledAmount, weights);
   } catch (error) {
     // TODO: Add Sentry
     throw new Error('Unsuccessful deposit simulation.');
   }
 
   // Estimate gas
-  if (isETH) {
-    const gasLimit = await mellowRouter.estimateGas.depositEth(weights, tempOverrides);
-    tempOverrides.gasLimit = getGasBuffer(gasLimit);
-  } else {
-    const gasLimit = await mellowRouter.estimateGas.depositErc20(
-      scaledAmount,
-      weights,
-      tempOverrides,
-    );
-    tempOverrides.gasLimit = getGasBuffer(gasLimit);
+  const gasLimit = await mellowRouter.estimateGas.depositErc20(scaledAmount, weights);
+  tempOverrides.gasLimit = getGasBuffer(gasLimit);
+
+  // Send transaction
+  return mellowRouter.depositErc20(scaledAmount, weights, tempOverrides);
+};
+
+export const deposit = async ({
+  routerId,
+  amount,
+  spareWeights,
+  signer,
+}: DepositArgs): Promise<DepositResponse> => {
+  // Get Mellow Config
+  const routerConfig = getRouterConfig(routerId);
+
+  // Deposit is only allowed for routers
+  if (routerConfig.isVault) {
+    throw new Error('Deposit not supported for vaults.');
   }
 
-  const tx = isETH
-    ? await mellowRouter.depositEth(weights, tempOverrides)
-    : await mellowRouter.depositErc20(scaledAmount, weights, tempOverrides);
+  // Get Router contract
+  const mellowRouter = new ethers.Contract(routerId, MellowMultiVaultRouterABI, signer);
 
+  // Get token address
+  const tokenId = await mellowRouter.token();
+
+  // Get token name and decimals
+  const { decimals: tokenDecimals, name: tokenName } = getTokenInfo(tokenId);
+  const isETH = tokenName === 'ETH';
+
+  // Scale the amount
+  const scaledAmount = scale(amount, tokenDecimals);
+
+  // Map spare weights to array
+  const weights = mapWeights(routerConfig, spareWeights);
+
+  // Get the transaction and wait for the receipt
+  const tx = await getTransaction(mellowRouter, isETH, scaledAmount, weights);
   const receipt = await tx.wait();
-  return receipt;
+
+  // Get the next state of the router
+  const userAddress = await signer.getAddress();
+  const routerInfo = await getOptimiserInfo(routerId, userAddress);
+
+  // Return the response
+  return {
+    transaction: {
+      receipt,
+    },
+    newRouterState: routerInfo,
+  };
 };
