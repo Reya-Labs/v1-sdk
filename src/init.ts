f6560dc54b0be5669390e36ac39b9adf164c1d95
tudor47 <tudorcostin7@gmail.com>
2023-01-20 14:40:47 +0200

    feat: added utils

--- src/init.ts
+++ src/init.ts
@@ -4,10 +4,13 @@ import {
   defaultStackParser,
   defaultIntegrations,
 } from '@sentry/browser';
+import { ethers } from 'ethers';
+import { initMellowConfig } from './entities/mellow-stateless/config/config';
 
 let sentryTracker: BrowserClient | null = null;
+let provider: ethers.providers.JsonRpcProvider | null = null;
 
-export const init = (): void => {
+export const initSentryTracker = (): void => {
   sentryTracker = new BrowserClient({
     dsn: 'https://c170b1643b064f2cb57b2204e1e3bf5f@o4504239616294912.ingest.sentry.io/4504247590060032',
     transport: makeFetchTransport,
@@ -22,9 +25,32 @@ export const init = (): void => {
   });
 };
 
+export const initProvider = (providerURL: string): void => {
+  provider = new ethers.providers.JsonRpcProvider(providerURL);
+};
+
+export const init = ({ providerURL, network }: { providerURL: string; network: string }): void => {
+  // Initialize Sentry
+  initSentryTracker();
+
+  // Initialize JSON RPC Provider
+  initProvider(providerURL);
+
+  // Initialize Mellow Config
+  initMellowConfig(network);
+};
+
 export const getSentryTracker = (): BrowserClient => {
   if (!sentryTracker) {
-    throw new Error('Sentry tracker is not setup!');
+    throw new Error('Sentry tracker is not set up!');
   }
   return sentryTracker;
+};
+
+export const getProvider = (): ethers.providers.JsonRpcProvider => {
+  if (!provider) {
+    throw new Error('Provider is not set up!');
+  }
+
+  return provider;
 };
