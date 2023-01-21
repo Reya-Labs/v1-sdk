ab11f24770aa07e797bd52b41e6ba6c0a884b7cf
tudor47 <tudorcostin7@gmail.com>
2023-01-21 15:04:59 +0200

    feat: added tests for actions

---
+++ tests/mocha/mellow-stateless/withdraw.test.ts
@@ -0,0 +1,177 @@
+import { ethers, network, waffle } from 'hardhat';
+import { describe } from 'mocha';
+import * as sinon from 'sinon';
+import { BrowserClient } from '@sentry/browser';
+import { expect } from 'chai';
+import { descale } from '../../../src/utils/scaling';
+import * as initSDK from '../../../src/init';
+import * as initMellowConfig from '../../../src/entities/mellow-stateless/config/config';
+import { MockGoerliConfig } from './utils';
+import { withSigner } from '../../utils';
+import { withdraw } from '../../../src/entities/mellow-stateless/mellow-routers/withdraw';
+import { getMellowProduct } from '../../../src/entities/mellow-stateless/getters/getMellowProduct';
+import { IERC20MinimalABI, MellowDepositWrapperABI } from '../../../src/ABIs';
+
+const { provider } = waffle;
+const DELTA = 0.00001;
+
+describe('getRouters', () => {
+  const userAddress = '0xf8f6b70a36f4398f0853a311dc6699aba8333cc1';
+
+  const resetNetwork = async (blockNumber: number) => {
+    await network.provider.request({
+      method: 'hardhat_reset',
+      params: [
+        {
+          chainId: 5,
+          forking: {
+            jsonRpcUrl: process.env.GOERLI_URL,
+            blockNumber,
+          },
+        },
+      ],
+    });
+  };
+
+  const mock = async () => {
+    const block = 8344555;
+    await resetNetwork(block);
+
+    sinon.stub(initSDK, 'getSentryTracker').callsFake(
+      () =>
+        ({
+          captureException: () => undefined,
+          captureMessage: () => undefined,
+        } as unknown as BrowserClient),
+    );
+
+    sinon.stub(initSDK, 'getProvider').callsFake(() => provider);
+
+    sinon.stub(initMellowConfig, 'getMellowConfig').callsFake(() => MockGoerliConfig);
+  };
+
+  const restore = async () => {
+    // restore the original implementation of initSDK.getSentryTracker
+    (initSDK.getSentryTracker as sinon.SinonStub).restore();
+
+    // restore the original implementation of initSDK.getProvider
+    (initSDK.getProvider as sinon.SinonStub).restore();
+
+    // restore the original implementation of initMellowConfig.getMellowConfig
+    (initMellowConfig.getMellowConfig as sinon.SinonStub).restore();
+  };
+
+  beforeEach(async () => {
+    await mock();
+  });
+
+  afterEach(async () => {
+    await restore();
+  });
+
+  describe('ETH router withdraw', () => {
+    it('withdraw in vault active', async () => {
+      const routerId = '0x62E224d9ae2f4702CC88695e6Ea4aA16D0925BdB';
+      const vaultId = '0x62E224d9ae2f4702CC88695e6Ea4aA16D0925BdB';
+
+      await withSigner(network, userAddress, async (signer) => {
+        // Get ERC20 vault contract
+        const wrapperId = '0xcF2f79d8DF97E09BF5c4DBF3F953aeEF4f4a204d';
+        const ethWrapper = new ethers.Contract(wrapperId, MellowDepositWrapperABI, signer);
+
+        // Make a deposit (since current deposit is 0)
+        await ethWrapper.deposit(routerId, 0, [], {
+          value: '100000000000000000',
+        });
+
+        // Get the token contract
+        const wethContract = new ethers.Contract(
+          '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6',
+          IERC20MinimalABI,
+          signer,
+        );
+
+        const balance = descale(await wethContract.balanceOf(userAddress), 18);
+
+        const routerState = await getMellowProduct({
+          routerId,
+          userAddress,
+        });
+
+        await withdraw({
+          routerId,
+          vaultId,
+          signer,
+        });
+
+        const newBalance = descale(await wethContract.balanceOf(userAddress), 18);
+
+        const { newRouterState } = await withdraw({
+          routerId,
+          vaultId,
+          signer,
+        });
+
+        // Nothing to withdraw, change 0
+        expect(newRouterState.userRouterDeposit - routerState.userRouterDeposit).to.be.closeTo(
+          -0.1,
+          DELTA,
+        );
+
+        // Nothing to withdraw, change 0
+        expect(newBalance - balance).to.be.closeTo(0.1, DELTA);
+      });
+    });
+
+    it('withdraw in ETH', async () => {
+      const routerId = '0x704F6E9cB4f7e041CC89B6a49DF8EE2027a55164';
+      const vaultId = '0xca5ecDeb7f6E3E6d40E685ac49E76aC8EeE7049B';
+
+      await withSigner(network, userAddress, async (signer) => {
+        const routerState = await getMellowProduct({
+          routerId,
+          userAddress,
+        });
+
+        const { newRouterState } = await withdraw({
+          routerId,
+          vaultId,
+          signer,
+        });
+
+        expect(newRouterState.userRouterDeposit - routerState.userRouterDeposit).to.be.closeTo(
+          -0.022,
+          DELTA,
+        );
+      });
+    });
+
+    it('withdraw in USDC', async () => {
+      const routerId = '0x9f397CD24103A0a0252DeC82a88e656480C53fB7';
+      const vaultId = '0xAb0f8CeE5fa7e3D577d1a546Aeb11fE5c768c75E';
+
+      await withSigner(network, userAddress, async (signer) => {
+        const routerState = await getMellowProduct({
+          routerId,
+          userAddress,
+        });
+
+        const { newRouterState } = await withdraw({
+          routerId,
+          vaultId,
+          signer,
+        });
+
+        expect(newRouterState.userRouterDeposit - routerState.userRouterDeposit).to.be.closeTo(
+          -27.3,
+          DELTA,
+        );
+
+        expect(newRouterState.userWalletBalance - routerState.userWalletBalance).to.be.closeTo(
+          27.3,
+          DELTA,
+        );
+      });
+    });
+  });
+});
