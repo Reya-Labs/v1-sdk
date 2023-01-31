9bfc50e72d16f945684d56c90da6db8915f6aeb0
Ioana Roceanu <ioanaroceanu@gmail.com>
2023-01-31 11:32:35 +0000

    chore: add aUSDC v6 config to mellow

--- src/entities/mellow/config/config.ts
+++ src/entities/mellow/config/config.ts
@@ -49,7 +49,7 @@ const networkConfigurations: { [key: string]: NetworkConfiguration } = {
               withdrawable: true,
             },
             {
-              weight: 25,
+              weight: 50,
               pools: ['AAVE - USDC BORROW'],
               maturityTimestampMS: 1680264000000,
               estimatedHistoricApy: [0, 20],
@@ -63,7 +63,14 @@ const networkConfigurations: { [key: string]: NetworkConfiguration } = {
               withdrawable: false,
             },
             {
-              weight: 75,
+              weight: 25,
+              pools: ['AAVE - USDC LEND'],
+              maturityTimestampMS: 1677585600000,
+              estimatedHistoricApy: [0, 20],
+              withdrawable: false,
+            },
+            {
+              weight: 25,
               pools: ['AAVE - USDC LEND'],
               maturityTimestampMS: 1680264000000,
               estimatedHistoricApy: [0, 20],
