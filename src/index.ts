f99be1e308d5424ce7e8c69b1b033f6ff41c8967
ssbarbee <ssbarbee@users.noreply.github.com>
2023-02-10 14:29:42 +0100

    fix: export name

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from './entities';
 export * from './services';
-export { SupportedChainId as SupportedNetworksEnum } from './types';
+export { SupportedChainId } from './types';
 export { init, initV1, getProvider, getProviderV1, getSubgraphURL } from './init';
