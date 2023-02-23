464882e8a2c4b30afd8f767637e744bf8be02ab1
Ioana Roceanu <ioanaroceanu@gmail.com>
2023-02-23 11:30:49 +0000

    fix: integrate subgraph-data & fix tests

---
+++ src/entities/amm/getters/historicalRates/getHistoricalRate.ts
@@ -0,0 +1,101 @@
+import {
+  getHistoricalFixedRate as getTickUpdates,
+  getHistoricalVariableIndex,
+} from '@voltz-protocol/subgraph-data';
+import { BigNumber } from 'ethers';
+
+export enum Granularity {
+  ONE_HOUR = 3600 * 1000,
+  ONE_DAY = 86400 * 1000,
+  ONE_WEEK = 604800 * 1000,
+}
+
+export type HistoricalRates = {
+  timestampInMs: number;
+  value: number;
+};
+
+export const getHistoricalRates = async (
+  subgraphUrl: string,
+  isFixed: boolean,
+  filters: {
+    granularity: Granularity;
+    timeframeMs: number;
+  },
+  ammId?: string,
+  rateOracleId?: string,
+): Promise<HistoricalRates[]> => {
+  // check ids
+  let parentObjectId;
+  if (isFixed && ammId) {
+    parentObjectId = ammId;
+  } else if (!isFixed && rateOracleId) {
+    parentObjectId = rateOracleId;
+  } else {
+    throw new Error('Unable to get rates, parent object not provided');
+  }
+
+  // get ticks (with timeframe)
+  const currentTimestamp = Date.now();
+
+  const startTime = currentTimestamp - filters.timeframeMs;
+  const endTime = currentTimestamp;
+
+  // subgraph-data should output points in asc order by timestamp
+  const rateUpdates = await getSubgraphData(
+    subgraphUrl,
+    isFixed,
+    parentObjectId,
+    Math.round(startTime / 1000),
+    Math.round(endTime / 1000),
+  );
+
+  const result = [];
+
+  // NOTE: points will be registered exactly every hour on the ETHEREUM mainnet
+  // but on chains with arbitrary block interval, this won't be the case
+  let latestParsedTimestamp = 0;
+  for (const p of rateUpdates) {
+    if (
+      latestParsedTimestamp + filters.granularity <= p.timestampInMs &&
+      p.timestampInMs >= startTime &&
+      p.timestampInMs <= endTime
+    ) {
+      result.push({
+        timestampInMs: p.timestampInMs,
+        value: p.value.div(BigNumber.from(1000000000000)).toNumber() / 1000000,
+      });
+      latestParsedTimestamp = p.timestampInMs;
+    }
+  }
+
+  return result;
+};
+
+export const getSubgraphData = async (
+  subgraphUrl: string,
+  isFixed: boolean,
+  parentObjectId: string,
+  startTime: number,
+  endTime: number,
+): Promise<
+  {
+    timestampInMs: number;
+    value: BigNumber;
+  }[]
+> => {
+  if (isFixed) {
+    const res = await getTickUpdates(subgraphUrl, parentObjectId, startTime, endTime);
+    const mappedResult = res.map((r) => ({
+      timestampInMs: r.timestampInMS,
+      value: r.historicalFixedRate,
+    }));
+    return mappedResult;
+  }
+  const res = await getHistoricalVariableIndex(subgraphUrl, parentObjectId, startTime, endTime);
+  const mappedResult = res.map((r) => ({
+    timestampInMs: r.timestampInMS,
+    value: r.historicalVariableRate,
+  }));
+  return mappedResult;
+};
--- tests/mocha/amm-stateless/getHistoricalRates.test.ts
+++ tests/mocha/amm-stateless/getHistoricalRates.test.ts
@@ -3,153 +3,14 @@ import { expect } from 'chai';
 import * as sinon from 'sinon';
 import { BrowserClient } from '@sentry/browser';
 import * as initSDK from '../../../src/init';
-import { getHistoricalVariableRate, getHistoricalFixedRates } from '../../../src/entities';
-import * as fun from '../../../src/entities/amm/getters/historicalRates/getHistoricalVariableRate';
-import * as funFixed from '../../../src/entities/amm/getters/historicalRates/getHistoricalFixedRate';
-import { RateUpdate, TickUpdate } from '@voltz-protocol/subgraph-data';
-import { ONE_DAY_IN_SECONDS, ONE_YEAR_IN_SECONDS, WAD } from '../../../src/constants';
+import { getHistoricalRates } from '../../../src/entities';
+import * as fun from '../../../src/entities/amm/getters/historicalRates/getHistoricalRate';
+import { ONE_DAY_IN_SECONDS } from '../../../src/constants';
 import { BigNumber } from 'ethers';
 
-describe.skip('getHistoricalVariableRate', () => {
-  const mock = async (observations: RateUpdate[]) => {
-    sinon.stub(initSDK, 'getSentryTracker').callsFake(
-      () =>
-        ({
-          captureException: () => undefined,
-          captureMessage: () => undefined,
-        } as unknown as BrowserClient),
-    );
-    
-    sinon.stub(fun, 'getSubgraphData').onFirstCall().resolves(observations);
-  };
 
-  const restore = async () => {
-    sinon.restore();
-  };
-
-  afterEach(async () => {
-    await restore();
-  });
-
-  it('start time earlier than observations', async () => {
-    const currentTimestamp = Date.now();
-    const obseravtions : RateUpdate[] = [
-        {id: "mockId1", rate: WAD, timestampInMS: currentTimestamp - ONE_DAY_IN_SECONDS * 2 * 1000},
-        {id: "mockId2", rate: WAD.add(WAD.div(3)), timestampInMS: currentTimestamp - ONE_DAY_IN_SECONDS * 1 * 1000},
-        {id: "mockId2", rate: WAD.add(WAD.div(2)), timestampInMS: currentTimestamp},
-    ];
-    await mock(obseravtions);
-
-    const promise = getHistoricalVariableRate(
-        "mockUrl",
-        "mockId", 
-        {
-            granularityMs: ONE_DAY_IN_SECONDS * 0.5 * 1000, 
-            timeframeMs: ONE_DAY_IN_SECONDS * 4 * 1000
-        }
-    );
-
-    expect(Promise.resolve(promise)).to.be.revertedWith("Timeframe spans past the set's initial observation")
-  });
-
-  it('end time later than observations', async () => {
-    const currentTimestamp = Date.now();
-    const obseravtions : RateUpdate[] = [
-        {id: "mockId1", rate: WAD, timestampInMS: currentTimestamp - ONE_DAY_IN_SECONDS * 2 * 1000},
-        {id: "mockId2", rate: WAD.add(WAD.div(3000)), timestampInMS: currentTimestamp - ONE_DAY_IN_SECONDS * 1 * 1000},
-        {id: "mockId3", rate: WAD.add(WAD.div(2000)), timestampInMS: currentTimestamp},
-    ];
-    await mock(obseravtions);
-
-    const result : {apy: number, timestampMs: number}[] = await getHistoricalVariableRate(
-        "mockUrl",
-        "mockId", 
-        {
-            granularityMs: ONE_DAY_IN_SECONDS * 0.5 * 1000, 
-            timeframeMs: ONE_DAY_IN_SECONDS * 2 * 1000
-        }
-    );
-    expect(result.length).to.be.eq(5);
-  });
-
-  it('no observations', async () => {
-    const obseravtions : RateUpdate[] = [];
-    await mock(obseravtions);
-
-    const promise =  getHistoricalVariableRate(
-        "mockUrl",
-        "mockId", 
-        {
-            granularityMs: ONE_DAY_IN_SECONDS * 0.5 * 1000, 
-            timeframeMs: ONE_DAY_IN_SECONDS * 2 * 1000
-        }
-    );
-    expect(Promise.resolve(promise)).to.be.revertedWith("Not enough observations")
-  });
-
-  it('little observation', async () => {
-    const currentTimestamp = Date.now();
-    const obseravtions : RateUpdate[] = [
-        {id: "mockId1", rate: WAD, timestampInMS: currentTimestamp - ONE_DAY_IN_SECONDS * 3 * 1000},
-        {id: "mockId2", rate: WAD.add(WAD.div(3000)), timestampInMS: currentTimestamp - ONE_DAY_IN_SECONDS * 2 * 1000},
-    ];
-    await mock(obseravtions);
-
-    const result : {apy: number, timestampMs: number}[] = await getHistoricalVariableRate(
-        "mockUrl",
-        "mockId", 
-        {
-            granularityMs: ONE_DAY_IN_SECONDS * 0.5 * 1000, 
-            timeframeMs: ONE_DAY_IN_SECONDS * 3 * 1000
-        }
-    );
-    expect(result.length).to.be.eq(7);
-  });
-
-  it('correct interpolation', async () => {
-    const currentTimestamp = Date.now();
-    const obseravtions : RateUpdate[] = [
-        {id: "mockId1", rate: WAD, timestampInMS: currentTimestamp - ONE_YEAR_IN_SECONDS * 1000},
-        {id: "mockId2", rate: WAD.add(WAD), timestampInMS: currentTimestamp},
-    ];
-    await mock(obseravtions);
-
-    const result : {apy: number, timestampMs: number}[] = await getHistoricalVariableRate(
-        "mockUrl",
-        "mockId", 
-        {
-            granularityMs: ONE_YEAR_IN_SECONDS * 0.5 * 1000, 
-            timeframeMs: ONE_YEAR_IN_SECONDS * 1000
-        }
-    );
-    expect(result.length).to.be.eq(3);
-    expect(result[1].apy).to.be.closeTo(100, 1)
-  });
-
-  it('correct extrapolation', async () => {
-    const currentTimestamp = Date.now();
-    const obseravtions : RateUpdate[] = [
-        {id: "mockId1", rate: WAD, timestampInMS: currentTimestamp - ONE_YEAR_IN_SECONDS * 2 * 1000},
-        {id: "mockId2", rate: WAD.add(WAD.div(20)), timestampInMS: currentTimestamp - ONE_YEAR_IN_SECONDS * 1000},
-    ];
-    await mock(obseravtions);
-
-    const result : {apy: number, timestampMs: number}[] = await getHistoricalVariableRate(
-        "mockUrl",
-        "mockId", 
-        {
-            granularityMs: ONE_YEAR_IN_SECONDS * 0.5 * 1000, 
-            timeframeMs: ONE_YEAR_IN_SECONDS * 2 * 1000
-        }
-    );
-    expect(result.length).to.be.eq(5);
-    expect(result[3].apy).to.be.closeTo(5, 0.01)
-  });
-
-});
-
-describe('getHistoricalFixedRate', () => {
-    const mock = async (observations: TickUpdate[]) => {
+describe('getHistoricalRates', () => {
+    const mock = async (observations: any[]) => {
       sinon.stub(initSDK, 'getSentryTracker').callsFake(
         () =>
           ({
@@ -158,7 +19,7 @@ describe('getHistoricalFixedRate', () => {
           } as unknown as BrowserClient),
       );
       
-      sinon.stub(funFixed, 'getSubgraphData').resolves(observations);
+      sinon.stub(fun, 'getSubgraphData').resolves(observations);
     };
   
     const restore = async () => {
@@ -171,120 +32,183 @@ describe('getHistoricalFixedRate', () => {
   
     it('start time earlier than observations', async () => {
       const currentTimestamp = Date.now();
-      const obseravtions : TickUpdate[] = [
-          {id: "mockId2", tick: BigNumber.from(6960), timestampInMS: currentTimestamp - ONE_DAY_IN_SECONDS * 1 * 1000},
+      const obseravtions = [
+          {value: BigNumber.from("47400000000000000"), timestampInMs: currentTimestamp - fun.Granularity.ONE_DAY},
       ];
       await mock(obseravtions);
   
-      const result : {rate: number, timestampMs: number}[] = await getHistoricalFixedRates(
+      const resultFixed : fun.HistoricalRates[] = await getHistoricalRates(
           "mockUrl",
-          "mockId", 
+          true,
           {
-              granularityMs: ONE_DAY_IN_SECONDS * 0.5 * 1000, 
+              granularity: fun.Granularity.ONE_DAY, 
               timeframeMs: ONE_DAY_IN_SECONDS * 2 * 1000
-          }
+          },
+          "mockId", 
+          undefined
       );
   
-      console.log(result);
-      expect(result.length).to.be.eq(5);
-      expect(result[0].rate).to.be.eq(1);
+      expect(resultFixed.length).to.be.eq(1);
+      expect(resultFixed[0].value).to.be.eq(0.0474);
+
+      const resultVariable : fun.HistoricalRates[] = await getHistoricalRates(
+        "mockUrl",
+        false,
+        {
+            granularity: fun.Granularity.ONE_DAY, 
+            timeframeMs: ONE_DAY_IN_SECONDS * 2 * 1000
+        },
+        undefined, 
+        "mockId"
+      );
+
+      expect(resultVariable.length).to.be.eq(1);
+      expect(resultVariable[0].value).to.be.eq(0.0474);
     });
   
-    it('end time later than observations', async () => {
+    // this case should not happen, the subgraph should never return data is there isn't any
+    it('start time later than observations', async () => {
         const currentTimestamp = Date.now();
-        const obseravtions : TickUpdate[] = [
-            {id: "mockId2", tick: BigNumber.from(6960), timestampInMS: currentTimestamp - ONE_DAY_IN_SECONDS * 1 * 1000},
-            {id: "mockId2", tick: BigNumber.from(0), timestampInMS: currentTimestamp},
+        const obseravtions = [
+            {value: BigNumber.from("47400000000000000"), timestampInMs: currentTimestamp - fun.Granularity.ONE_DAY * 2},
+            {value: BigNumber.from("49100000000000000"), timestampInMs: currentTimestamp - fun.Granularity.ONE_DAY - 60000},
         ];
         await mock(obseravtions);
     
-        const result : {rate: number, timestampMs: number}[] = await getHistoricalFixedRates(
-            "mockUrl",
-            "mockId", 
+        const resultFixed : fun.HistoricalRates[] = await getHistoricalRates(
+            "mockUrl", 
+            true,
             {
-                granularityMs: ONE_DAY_IN_SECONDS * 0.5 * 1000, 
+                granularity: fun.Granularity.ONE_DAY, 
                 timeframeMs: ONE_DAY_IN_SECONDS * 1 * 1000
-            }
+            },
+            "mockId",
+            undefined
         );
     
-        console.log(result);
-        expect(result.length).to.be.eq(3);
-        expect(result[2].rate).to.be.eq(1);
+        expect(resultFixed.length).to.be.eq(0);
+
+        const resultVariable : fun.HistoricalRates[] = await getHistoricalRates(
+            "mockUrl",
+            false,
+            {
+                granularity: fun.Granularity.ONE_DAY, 
+                timeframeMs: ONE_DAY_IN_SECONDS * 1 * 1000
+            },
+            undefined, 
+            "mockId"
+        );
+
+        expect(resultVariable.length).to.be.eq(0);
     });
   
     it('many observations & big granularity', async () => {
-        const currentTimestamp = Date.now();
-        const obseravtions : TickUpdate[] = [
-            {id: "mockId2", tick: BigNumber.from(6900), timestampInMS: currentTimestamp - ONE_DAY_IN_SECONDS * 2 * 1000},
-            {id: "mockId2", tick: BigNumber.from(0), timestampInMS: currentTimestamp - ONE_DAY_IN_SECONDS * 1.5 * 1000},
-            {id: "mockId2", tick: BigNumber.from(6960), timestampInMS: currentTimestamp - ONE_DAY_IN_SECONDS * 1 * 1000},
-            {id: "mockId2", tick: BigNumber.from(6000), timestampInMS: currentTimestamp - ONE_DAY_IN_SECONDS * 0.5 * 1000},
-            {id: "mockId2", tick: BigNumber.from(5940), timestampInMS: currentTimestamp},
+        const currentTimestamp = Date.now() + 1000;
+        const obseravtions = [
+            {value: BigNumber.from("50000000000000000"), timestampInMs: currentTimestamp - ONE_DAY_IN_SECONDS * 2 * 1000},
+            {value: BigNumber.from("40000000000000000"), timestampInMs: currentTimestamp - ONE_DAY_IN_SECONDS * 1.5 * 1000},
+            {value: BigNumber.from("47000000000000000"), timestampInMs: currentTimestamp - ONE_DAY_IN_SECONDS * 1 * 1000},
+            {value: BigNumber.from("400000000000000000"), timestampInMs: currentTimestamp - ONE_DAY_IN_SECONDS * 0.5 * 1000},
+            {value: BigNumber.from("1100000000000000"), timestampInMs: currentTimestamp},
         ];
         await mock(obseravtions);
     
-        const result : {rate: number, timestampMs: number}[] = await getHistoricalFixedRates(
-            "mockUrl",
-            "mockId", 
+        const resultFixed : fun.HistoricalRates[] = await getHistoricalRates(
+            "mockUrl", 
+            true,
             {
-                granularityMs: ONE_DAY_IN_SECONDS * 1000, 
+                granularity: fun.Granularity.ONE_DAY, 
                 timeframeMs: ONE_DAY_IN_SECONDS * 2 * 1000
-            }
+            },
+            "mockId",
+            undefined
         );
     
-        console.log(result);
-        expect(result.length).to.be.eq(3);
-        expect(result[0].rate).to.be.eq(1/(1.0001 ** 6900));
-        expect(result[1].rate).to.be.eq(1/(1.0001 ** 6960));
-        expect(result[2].rate).to.be.eq(1/(1.0001 ** 5940));
+        expect(resultFixed.length).to.be.eq(2);
+        expect(resultFixed[0].value).to.be.eq(0.05);
+        expect(resultFixed[1].value).to.be.eq(0.047);
+
+        const resultVariable : fun.HistoricalRates[] = await getHistoricalRates(
+            "mockUrl",
+            false,
+            {
+                granularity: fun.Granularity.ONE_DAY, 
+                timeframeMs: ONE_DAY_IN_SECONDS * 2 * 1000
+            },
+            undefined, 
+            "mockId"
+        );
+
+        expect(resultVariable.length).to.be.eq(2);
+        expect(resultVariable[0].value).to.be.eq(0.05);
+        expect(resultVariable[1].value).to.be.eq(0.047);
     });
   
     it('little observation & smaller granularity', async () => {
         const currentTimestamp = Date.now();
-        const obseravtions : TickUpdate[] = [
-            {id: "mockId2", tick: BigNumber.from(6960), timestampInMS: currentTimestamp - ONE_DAY_IN_SECONDS * 1 * 1000},
+        const obseravtions = [
+            {value: BigNumber.from("47000000000000000"), timestampInMs: currentTimestamp - ONE_DAY_IN_SECONDS * 1 * 1000},
         ];
         await mock(obseravtions);
     
-        const result : {rate: number, timestampMs: number}[] = await getHistoricalFixedRates(
-            "mockUrl",
-            "mockId", 
+        const resultFixed : fun.HistoricalRates[] = await getHistoricalRates(
+            "mockUrl", 
+            true,
             {
-                granularityMs: ONE_DAY_IN_SECONDS * 0.5 * 1000, 
-                timeframeMs: ONE_DAY_IN_SECONDS * 2 * 1000
-            }
+                granularity: fun.Granularity.ONE_DAY, 
+                timeframeMs: ONE_DAY_IN_SECONDS * 4 * 1000
+            },
+            "mockId",
+            undefined
         );
     
-        console.log(result);
-        expect(result.length).to.be.eq(5);
-        expect(result[0].rate).to.be.eq(1);
-        expect(result[1].rate).to.be.eq(1);
-        expect(result[2].rate).to.be.eq(1/(1.0001 ** 6960));
-        expect(result[3].rate).to.be.eq(1/(1.0001 ** 6960));
-        expect(result[4].rate).to.be.eq(1/(1.0001 ** 6960));
+        expect(resultFixed.length).to.be.eq(1);
+        expect(resultFixed[0].value).to.be.eq(0.047);
+
+        const resultVariable : fun.HistoricalRates[] = await getHistoricalRates(
+            "mockUrl",
+            false,
+            {
+                granularity: fun.Granularity.ONE_DAY, 
+                timeframeMs: ONE_DAY_IN_SECONDS * 2 * 1000
+            },
+            undefined, 
+            "mockId"
+        );
+
+        expect(resultVariable.length).to.be.eq(1);
+        expect(resultVariable[0].value).to.be.eq(0.047);
     });
   
     it('no observations', async () => {
         const currentTimestamp = Date.now();
-        const obseravtions : TickUpdate[] = [];
+        const obseravtions : { timestampInMs: number; value: BigNumber}[] = [];
         await mock(obseravtions);
     
-        const result : {rate: number, timestampMs: number}[] = await getHistoricalFixedRates(
-            "mockUrl",
-            "mockId", 
+        const resultFixed : fun.HistoricalRates[] = await getHistoricalRates(
+            "mockUrl", 
+            true,
             {
-                granularityMs: ONE_DAY_IN_SECONDS * 0.5 * 1000, 
+                granularity: fun.Granularity.ONE_DAY, 
                 timeframeMs: ONE_DAY_IN_SECONDS * 2 * 1000
-            }
+            },
+            "mockId",
+            undefined
         );
     
-        console.log(result);
-        expect(result.length).to.be.eq(5);
-        expect(result[0].rate).to.be.eq(1);
-        expect(result[1].rate).to.be.eq(1);
-        expect(result[2].rate).to.be.eq(1);
-        expect(result[3].rate).to.be.eq(1);
-        expect(result[4].rate).to.be.eq(1);
+        expect(resultFixed.length).to.be.eq(0);
+
+        const resultVariable : fun.HistoricalRates[] = await getHistoricalRates(
+            "mockUrl",
+            false,
+            {
+                granularity: fun.Granularity.ONE_DAY, 
+                timeframeMs: ONE_DAY_IN_SECONDS * 2 * 1000
+            },
+            undefined, 
+            "mockId"
+        );
+        expect(resultVariable.length).to.be.eq(0);
     });
   
 });
