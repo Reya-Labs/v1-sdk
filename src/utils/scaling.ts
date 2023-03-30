085b8f05b71211a915c995bd7000d37abda6f3ea
Alexandru Niculae <niculae.alexandru1@gmail.com>
2023-03-30 14:14:15 +0300

    fix: token allowance descaling

--- src/utils/scaling.ts
+++ src/utils/scaling.ts
@@ -7,11 +7,3 @@ export const descale = (amount: BigNumberish, decimals: number): number => {
 export const scale = (amount: number, decimals: number): BigNumber => {
   return ethers.utils.parseUnits(amount.toFixed(decimals), decimals);
 };
-
-export const descaleToString = (amount: BigNumberish, decimals: number): string => {
-  return ethers.utils.formatUnits(amount, decimals);
-};
-
-export const descaleToBigNumber = (amount: BigNumberish, decimals: number): BigNumber => {
-  return BigNumber.from(descaleToString(amount, decimals).split('.')[0]);
-};
