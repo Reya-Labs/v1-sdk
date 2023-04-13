cee049e86689578d23f42ca0219a987ef384c043
Artur Begyan <arturbegyan98@gmail.com>
2023-04-13 02:17:41 +0100

    feat: get chain level information

---
+++ src/services/getChainLevelInformation.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import { getSentryTracker } from '../init';
+
+const baseURL = 'https://voltz-indexer-3wpwbm66ca-nw.a.run.app/api/amms';
+
+export const getChainLevelInformation = async (
+  chainId: number,
+): Promise<{
+  volume30DayInDollars: number;
+  totalLiquidityInDollars: number;
+}> => {
+  try {
+    const url = `${baseURL}/${chainId}`;
+    const res = await axios({
+      method: 'get',
+      url: url,
+      withCredentials: false,
+    });
+    return {
+      volume30DayInDollars: res.data.volume30Day,
+      totalLiquidityInDollars: res.data.totalLiquidity,
+    };
+  } catch (e) {
+    const sentryTracker = getSentryTracker();
+    sentryTracker.captureMessage('GCloud Amm Information API unavailable');
+
+    return {
+      volume30DayInDollars: 0,
+      totalLiquidityInDollars: 0,
+    };
+  }
+};
