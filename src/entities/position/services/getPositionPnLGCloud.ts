0d1d39589363057cdeb9a97ccd6ad46f096406de
Artur Begyan <arturbegyan98@gmail.com>
2023-04-14 19:20:00 +0100

    feat: add return type to get position pnl gcloud

--- src/entities/position/services/getPositionPnLGCloud.ts
+++ src/entities/position/services/getPositionPnLGCloud.ts
@@ -4,17 +4,19 @@ import { SupportedChainId } from '../../../types';
 
 const baseURL = 'http://34.142.8.201:8080/api/positions';
 
+export type GetPositionPnLGCloudReturn = {
+  realizedPnLFromSwaps: number;
+  realizedPnLFromFeesPaid: number;
+  unrealizedPnLFromSwaps: number;
+};
+
 export const getPositionPnLGCloud = async (
   chainId: SupportedChainId,
   vammAddress: string,
   ownerAddress: string,
   tickLower: number,
   tickUpper: number,
-): Promise<{
-  realizedPnLFromSwaps: number;
-  realizedPnLFromFeesPaid: number;
-  unrealizedPnLFromSwaps: number;
-}> => {
+): Promise<GetPositionPnLGCloudReturn> => {
   try {
     const url = `${baseURL}/${chainId}/${vammAddress}/${ownerAddress}/${tickLower}/${tickUpper}`;
     const res = await axios({
