b798ef6cf4493170b3779201a6dfca2a0a44243d
Alexandru Niculae <niculae.alexandru1@gmail.com>
2023-05-18 10:48:35 +0100

    feat: get historical rates from voltz api

--- src/services/v1-indexer/urls.ts
+++ src/services/v1-indexer/urls.ts
@@ -1,6 +1,12 @@
 const baseUrl = 'https://api.voltz.xyz';
 
-type Service = 'chain-information' | 'all-pools' | 'position-pnl' | 'voyage';
+type Service =
+  | 'chain-information'
+  | 'all-pools'
+  | 'position-pnl'
+  | 'voyage'
+  | 'fixed-rates'
+  | 'variable-rates';
 
 export const getServiceUrl = (service: Service): string => {
   return `${baseUrl}/${service}`;
