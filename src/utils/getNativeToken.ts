471509ba93975ba1dd0d3dd2d061d421863be942
ssbarbee <ssbarbee@users.noreply.github.com>
2023-05-27 00:29:25 +0200

    refactor: improve code

--- src/utils/getNativeToken.ts
+++ src/utils/getNativeToken.ts
@@ -2,36 +2,41 @@ import { providers } from 'ethers';
 import { getSentryTracker } from '../init';
 import { SupportedChainId } from '../types';
 
+const CHAIN_TOKEN_MAP: Record<SupportedChainId, 'ETH' | 'AVAX'> = {
+  [SupportedChainId.mainnet]: 'ETH',
+  [SupportedChainId.goerli]: 'ETH',
+  [SupportedChainId.arbitrum]: 'ETH',
+  [SupportedChainId.arbitrumGoerli]: 'ETH',
+  [SupportedChainId.avalanche]: 'AVAX',
+  [SupportedChainId.avalancheFuji]: 'AVAX',
+};
 export async function getNativeToken(provider: providers.Provider): Promise<'ETH' | 'AVAX'> {
-  let chainId = 0;
+  let chainId: SupportedChainId | null = null;
 
   const attempts = 5;
   for (let attempt = 0; attempt < attempts; attempt += 1) {
     try {
       // eslint-disable-next-line no-await-in-loop
-      chainId = (await provider.getNetwork()).chainId;
+      chainId = (await provider.getNetwork()).chainId as SupportedChainId;
       break;
     } catch (error) {
       if (attempt + 1 === attempts) {
         const sentryTracker = getSentryTracker();
         sentryTracker.captureException(error);
-        sentryTracker.captureMessage('Unable to fetch gas price after 5 attempts');
+        sentryTracker.captureMessage('Unable to provider.getNetwork after 5 attempts');
       }
     }
   }
 
-  switch (chainId) {
-    case SupportedChainId.mainnet:
-    case SupportedChainId.goerli:
-    case SupportedChainId.arbitrum:
-    case SupportedChainId.arbitrumGoerli:
-      return 'ETH';
-
-    case SupportedChainId.avalanche:
-    case SupportedChainId.avalancheFuji:
-      return 'AVAX';
-
-    default:
-      throw Error(`Unsupported chain id ${chainId}`);
+  if (!chainId) {
+    const sentryTracker = getSentryTracker();
+    sentryTracker.captureMessage('Cannot detect chain id');
+    throw Error('Cannot detect chain id');
   }
+
+  if (CHAIN_TOKEN_MAP[chainId]) {
+    return CHAIN_TOKEN_MAP[chainId];
+  }
+
+  throw Error(`Unsupported chain id ${chainId}`);
 }
