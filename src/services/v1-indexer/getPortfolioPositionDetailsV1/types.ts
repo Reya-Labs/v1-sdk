7fa29e7397d56da358c2155ecb84fe1fc902ea8e
tudor47 <tudorcostin7@gmail.com>
2023-06-06 12:20:20 +0300

    feat: added maturity event

--- src/services/v1-indexer/getPortfolioPositionDetailsV1/types.ts
+++ src/services/v1-indexer/getPortfolioPositionDetailsV1/types.ts
@@ -1,7 +1,7 @@
 import { PortfolioPositionAMM } from '../getPortfolioPositionsV1';
 
 export type HistoryTransaction = {
-  type: 'swap' | 'mint' | 'burn' | 'margin-update' | 'liquidation' | 'settlement';
+  type: 'swap' | 'mint' | 'burn' | 'margin-update' | 'liquidation' | 'settlement' | 'maturity';
   creationTimestampInMS: number;
   notional: number;
   paidFees: number;
