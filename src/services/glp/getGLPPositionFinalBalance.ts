4e61df98c3ccd2c2084f5aa11546249fc58da857
Artur Begyan <arturbegyan98@gmail.com>
2023-07-02 16:09:01 +0100

    feat: get rid of csv in favour of json

--- src/services/glp/getGLPPositionFinalBalance.ts
+++ src/services/glp/getGLPPositionFinalBalance.ts
@@ -1,6 +1,4 @@
-import * as path from 'path';
-import * as fs from 'fs';
-import { parse } from 'csv-parse';
+import glpPositionsJSON from './glp_jun_28_2023_final_balances.json';
 
 export type GetPositionFinalBalanceArgs = {
   ownerAddress: string;
@@ -20,40 +18,7 @@ export const getGLPPositionFinalBalance = async ({
   tickLower,
   tickUpper,
 }: GetPositionFinalBalanceArgs): Promise<number> => {
-  const csvFilePath = path.resolve(__dirname, 'glp_jun_28_2023_final_balances.csv');
-  const headers = ['ownerAddress', 'tickLower', 'tickUpper', 'finalBalance'];
-  const fileContent = fs.readFileSync(csvFilePath, { encoding: 'utf-8' });
-  let positionFinalBalance = 0;
+  const json = glpPositionsJSON;
 
-  parse(
-    fileContent,
-    {
-      delimiter: ',',
-      columns: headers,
-    },
-    (error: any, result: FinalBalancesCSVRow[]) => {
-      if (error) {
-        console.error(error);
-      }
-      const positionRow: FinalBalancesCSVRow | undefined = result.find(
-        (row: FinalBalancesCSVRow): boolean => {
-          return (
-            row.ownerAddress === ownerAddress &&
-            Number(row.tickLower) === tickLower &&
-            Number(row.tickUpper) === tickUpper
-          );
-        },
-      );
-
-      if (!positionRow) {
-        throw new Error(
-          `No GLP 28th June position found for ownerAddress: ${ownerAddress}, tickLower: ${tickLower}, tickUpper: ${tickUpper}`,
-        );
-      }
-
-      positionFinalBalance = positionRow.finalBalance;
-    },
-  );
-
-  return positionFinalBalance;
+  return Number(json[0]['finalBalance']);
 };
